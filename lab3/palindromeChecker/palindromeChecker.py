0e58503e7cd454522fac40efef4cd67bffb7bf8e
sirflano <sirflano@gmail.com>
2015-09-30 15:34:32 +0000

    added comments and removed test prints

--- lab3/palindromeChecker/palindromeChecker.py
+++ lab3/palindromeChecker/palindromeChecker.py
@@ -1,12 +1,14 @@
 
 
 class readin:
+  #read in string
   teBeChecked = input("Input string to be checked please: ")
+  #strip string of whitespace
   teBeChecked = teBeChecked.replace(" ", "")
+  #copy a reverse of the sting
   tempString = teBeChecked[::-1]
-  print(teBeChecked)
-  print(tempString)
 
+  #check if the strings are the same, print result
   if teBeChecked == tempString:
     print("True")
   else:
